255ac6e804b943c2f57d85efcc2200edaac941b9
barrard <barrard@gmail.com>
2021-08-31 02:41:03 -0700

    port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("./rhubarb-server");
 const app = express();
 
+app.enable("trust proxy");
+app.set("trust proxy", 1);
 app.use(express.static("public"));
 
 app.all("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
